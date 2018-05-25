fed4067bc845611045aff4fd9d2b9d3b361fa08f
David <davidmrose@verizon.net>
2018-05-25 14:23:31 -0400

    Initial project.

---
+++ CherwellTest/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CherwellTest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void OnComputeVertices(object sender, RoutedEventArgs e)
+		{
+			var triangle = new Triangle(int.Parse(tbX.Text), int.Parse(tbY.Text));
+			triangle.ComputeCoordinates();
+			this.labelV1.Content = $"Coordinates: {triangle.ToString()}";
+
+			this.tbV1x.Text = triangle.V1.X.ToString();
+			this.tbV1y.Text = triangle.V1.Y.ToString();
+			this.tbV2x.Text = triangle.V2.X.ToString();
+			this.tbV2y.Text = triangle.V2.Y.ToString();
+			this.tbV3x.Text = triangle.V3.X.ToString();
+			this.tbV3y.Text = triangle.V3.Y.ToString();
+
+			this.labelRowCol.Content = $"Column (X): {triangle.VertexToColumn(triangle.V2)} : Row (Y): {triangle.VertexToRow(triangle.V2)}";
+
+			if (triangle.VertexToColumn(triangle.V2) == int.Parse(tbX.Text) && triangle.VertexToRow(triangle.V2) == int.Parse(tbY.Text))
+			{
+				labelSuccess.Content = "Success";
+			}
+			else
+			{
+				labelSuccess.Content = "No Match!!";
+			}
+		}
+
+		private void OnComputeRowColumn(object sender, RoutedEventArgs e)
+		{
+			this.tbV1x.Text = string.Empty;
+			this.tbV1y.Text = string.Empty;
+			this.tbV3x.Text = string.Empty;
+			this.tbV3y.Text = string.Empty;
+
+			var v2 = new Point(int.Parse(tbV2x.Text), int.Parse(tbV2y.Text));
+			//this.labelRowCol.Content = $"Column (X): {v2.VertexToColumn(v2.V2)} : Row (Y): {triangle.VertexToRow(triangle.V2)}";
+		}
+	}
+}
---
+++ CherwellTest/Triangle.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CherwellTest
+{
+	public class Triangle
+	{
+		private int Y;
+		private int X;
+		private bool isLowerTriangle;
+
+		public Triangle(int X, int Y)
+		{
+			this.Y = Y;
+			this.X = X;
+			isLowerTriangle = X % 2 != 0;
+		}
+
+		public Triangle(Point V1, Point V2, Point V3)
+		{
+			this.V1 = V1;
+			this.V2 = V2;
+			this.V3 = V3;
+			isLowerTriangle = V2.Y > V1.Y;
+		}
+
+		public void ComputeCoordinates()
+		{
+			if (isLowerTriangle)
+			{
+				int x = X - 1;
+				int offset = (x / 2);
+				V1 = new Point((x - offset) * 10, (Y - 1) * 10);
+				V2 = new Point(V1.X, V1.Y + 10);
+				V3 = new Point(V2.X + 10, V2.Y);
+			}
+			else
+			{
+				int x = X - 2;
+				int offset = (x / 2);
+				V1 = new Point((x - offset) * 10, (Y - 1) * 10);
+				V2 = new Point(V1.X + 10, V1.Y);
+				V3 = new Point(V2.X, V2.Y + 10);
+			}
+		}
+
+		public int VertexToColumn(Point vertex)
+		{
+			int x = vertex.X;
+
+			if (isLowerTriangle)
+			{
+				return 2 * (x / 10) + 1;
+			}
+			else
+			{
+				return 2 * (x / 10);
+			}
+		}
+
+		public int VertexToRow(Point vertex)
+		{
+			if (isLowerTriangle)
+			{
+				return vertex.Y / 10;
+			}
+			else
+			{
+				return vertex.Y / 10 + 1;
+			}
+		}
+
+		public Point V1 { get; private set; }
+
+		public Point V2 { get; private set; }
+
+		public Point V3 { get; private set; }
+
+		public override string ToString()
+		{
+			return $"V1 = {V1.ToString()}, V2 = {V2.ToString()}, V3 = {V3.ToString()}";
+		}
+	}
+}
